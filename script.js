1a26d27be54205ff5f8582c0b382647d56f9b787
Pradeep Pandey <90238672+UtechP-40@users.noreply.github.com>
2024-09-10 23:09:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,61 +1,24 @@
-let allBar = document.querySelectorAll(".bar");
+//your JS code here. If required.
+let prev_btn=document.querySelector("#prev");
+prev_btn.disabled=true;
+let next_btn=document.querySelector("#next");
+let hrz_line=document.querySelectorAll(".line");
+let circle=document.querySelectorAll(".circle");
+let line=0;
+next_btn.addEventListener("click",()=>{
+	prev_btn.disabled=false;
+	hrz_line[line].style.border="5px solid lightblue";
+	circle[line+1].style.border="5px solid lightblue";
+	line++;
+	if(line==4){
+		next_btn.disabled=true;
+	}
+	
+})
 
-let tillIndex = 0;
-// let color = "blue";
-
-function updateProgressBar() {
-    for (let i = 0; i < allBar.length; i++) {
-        
-        let s = allBar[i].querySelector(".circle");
-        let r = allBar[i].querySelector(".rect");
-        if (i < tillIndex) {
-            s.style.borderColor = "blue";
-            s.style.color = "blue";
-            if(r){
-                r.style.backgroundColor = "blue";
-            }
-        } else {
-            s.style.borderColor = "#4e4e4e";
-            s.style.color = "#4e4e4e";
-            if(r){
-                r.style.backgroundColor = "#4e4e4e";
-            }
-        }
-    }
-
-    if(tillIndex == 0){
-        prev.disabled = true;
-    }else{
-        prev.disabled = false;
-    }
-    if(tillIndex == allBar.length){
-        next.disabled = true;
-    }else{
-        next.disabled = false;
-    }
-}
-
-let next = document.querySelector("#next");
-let prev = document.querySelector("#prev");
-
-next.addEventListener("click", () => {
-    if (tillIndex < allBar.length) {
-        tillIndex++;
-        updateProgressBar();
-    }
-    console.log(allBar);
-console.log(tillIndex);
-});
-
-prev.addEventListener("click", () => {
-    if (tillIndex > 0) {
-        tillIndex--;
-        updateProgressBar();
-    }
-    
-    console.log(allBar);
-console.log(tillIndex);
-});
-
-// Initial update
-updateProgressBar();
+prev_btn.addEventListener("click",()=>{
+	next_btn.disabled=false;
+	hrz_line[line-1].style.border="5px solid grey";
+	circle[line].style.border="5px solid grey";
+	line--;
+})
